6d1e07d9cca70058df73fb0f58825d7b4e7bd7ad
Dnguy104 <dnguy104@ucr.edu>
2018-10-16 12:32:05 -0700

    geolocation and local storage

--- js/script.js
+++ js/script.js
@@ -1,67 +1,103 @@
 var timer = document.getElementById('timer');
 var toggleBtn = document.getElementById('toggle');
-var timeTbl = document.getElementById('timeTbl');
-
-var elem = {
-	timer: timer,
-	delay: 70
-}
-var watch = new Stopwatch(elem);
 
 var timeList = {
-	var times = [];
+	times: [],
+	addRow: function(start){
+		this.times.push({
+			startTime: '',
+			startLocation: '',
+			stopTime: '',
+			stopLocation: '',
+			elapsedTime: '',
+			isComplete: false;
+		});
+	},
+	addData: function(index, key, value) {
+		this.times[index][key] = value;
+	},
+	complete: function(index) {
+		this.times[index].complete = true;
+	}
 	
 }
 
-toggleBtn.addEventListener('click', function() {
-	if (!watch.isOn) {
-		var startTime = watch.start();
+var views = {
+	initializeList: function() {
+		var localS  = localStorage.getItem('times');
+		if(localS) {
+			 timeList.times = JSON.parse(localStorage.getItem('times'));
+		}
+		localStorage.setItem('times',JSON.stringify(timeList.times));
+		this.displayTimes();
+	},
+	displayTimes: function() {
+		var timeTbl = document.getElementById('timeTbl');
+		localStorage.setItem('times',JSON.stringify(timeList.times));
 		
-		handlers.start(startTime);
-		toggleBtn.textContent = 'Finish!';
-	}
-	else {
-		var endTime = watch.stop();
+		timeTbl.innerHTML = `<tr>
+								<th>Start Time</th>
+								<th>Start Location</th>
+								<th>Finished Time</th>
+								<th>Finish Location</th>
+								<th>Elapsed Time</th>
+							</tr>`;
+		timeList.times.forEach(function(elem1) {
+			var entry = document.createElement('tr');
+			for (var prop in elem1) {
+			    if (elem1.hasOwnProperty(prop)) {
+					var data = document.createElement('td');
+					data.innerHTML = elem1[prop];
+					entry.appendChild(data);
+					timeTbl.appendChild(entry);
+				}
+			}
+				
+		});
+	},
+	initializeBtn: function() {
+		toggleBtn.addEventListener('click', function() {
+			if (!watch.isOn) {
+				var startTime = watch.start();
+				
+				handlers.start(startTime);
+				toggleBtn.textContent = 'Finish!';
+			}
+			else {
+				var endTime = watch.stop();
 
-		handlers.stop(endTime);
-		toggleBtn.textContent = 'Start!';
+				handlers.stop(endTime);
+				toggleBtn.textContent = 'Start!';
+			}
+		});		
 	}
-});
+}
 
 var handlers = {
 	
 	start: function(time) {
-		handlers.addRow();
-		var newRow = timeTbl.lastChild.childNodes;
+		timeList.addRow(time);
+		var listSize = timeList.times.length - 1;
+		timeList.addData(listSize, 'startTime', time);
+		timeList.addData(listSize, 'startLocation', 'Loading...');
+		findLocation(listSize, 'startLocation');
 		
-		newRow[0].innerHTML = time;
-		newRow[1].innerHTML = 'Loading...'
-		findLocation(newRow[1]);
+		views.displayTimes();
 	},
 	
-	stop: function(time) {
-		var newRow = timeTbl.lastChild.childNodes;
+	stop: function(time) {		
+		var listSize = timeList.times.length - 1;
+		timeList.addData(listSize, 'stopTime', time['currentTime']);
+		timeList.addData(listSize, 'stopLocation', 'Loading...');
+		timeList.addData(listSize, 'elapsedTime', time['stopwatchTime']);
+		findLocation(listSize, 'stopLocation');
 		
-		newRow[2].innerHTML = time['currentTime'];
-		newRow[3].innerHTML = 'Loading...'
-		findLocation(newRow[3]);
-		newRow[4].innerHTML = time['stopwatchTime'];
-	},
-	
-	addRow: function() {
-		var entry = document.createElement('tr');
-		for(let i = 0; i < 5; i++) {
-			var data = document.createElement('td');
-			data.innerHTML = '<br><br>';
-			entry.appendChild(data);
-		}
-		
-		timeTbl.appendChild(entry);
+		views.displayTimes();
 	}
+	
 };
 
-
-function findLocation(location) {
+function findLocation(index, key) {
 	var output = document.getElementById('location');
 	
 	if (!navigator.geolocation){
@@ -72,15 +108,25 @@ function findLocation(location) {
 	function success(position) {
 		var latitude  = position.coords.latitude;
     	var longitude = position.coords.longitude;
-		
-		location.innerHTML = 'lat: ' + latitude.toFixed(4) + '<br>' + 'long: ' + longitude.toFixed(4);
+    	
+		timeList.addData(index, key, 'lat: ' + latitude.toFixed(4) + '<br>' + 'long: ' + longitude.toFixed(4));
+		views.displayTimes();
 	}
 	
 	function error() {
-	    location.innerHTML = 'lat: n/a' + '<br>' + 'long: n/a';
+	    timeList.addData(index, key, 'lat: n/a' + '<br>' + 'long: n/a');
+	    views.displayTimes();
 	}
 	
 	navigator.geolocation.getCurrentPosition(success, error);
 }
 
+var elem = {
+	timer: timer,
+	delay: 70
+}
+var watch = new Stopwatch(elem);
+
+views.initializeList();
+views.initializeBtn();
 
